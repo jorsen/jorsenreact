4ecdce26d31f279ca96e58b5b4e8c78c9dd166a9
jorsen <jorsenmejia@gmail.com>
2023-11-28 09:32:47 +0800

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,24 +20,11 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-if (isset($_ENV['DATABASE'])) {
-  define( 'DB_NAME', $_ENV['DATABASE'] );
-}
+define('DB_NAME', 'jorsenreactwp');
+define('DB_USER', 'k5a007ajxfax7lsjsq3a');
+define('DB_PASSWORD', 'pscale_pw_lYMl9JnpuulPS05jmkdhfOuqi9YDd4ya6TByPr6hYnE');
+define('DB_HOST', 'aws.connect.psdb.cloud'); // This is often 'localhost' or provided by your hosting provider
 
-/** Database username */
-if (isset($_ENV['USERNAME'])) {
-  define( 'DB_USER', $_ENV['USERNAME'] );
-}
-
-/** Database password */
-if (isset($_ENV['PASSWORD'])) {
-  define( 'DB_PASSWORD', $_ENV['PASSWORD'] );
-}
-
-/** Database hostname */
-if (isset($_ENV['HOST'])) {
-  define( 'DB_HOST', $_ENV['HOST'] );
-}
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -87,7 +74,7 @@ $table_prefix = isset($_ENV['TABLE_PREFIX']) ? $_ENV['TABLE_PREFIX'] : 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
